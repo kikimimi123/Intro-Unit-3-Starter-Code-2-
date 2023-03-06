6418730bfe0cca6e0b52a36aff7cbc7809d7d98f
Kiana Clercin <127120643+kikimimi123@users.noreply.github.com>
2023-03-06 14:44:26 +0000

    push

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let storyOpening = document.querySelector (".story-opening")
+let optionOneButton = document.querySelector(".option-one")
+
+let optionOneScreen = document.querySelector(".option-one-screen")
+
+
+ optionOneButton.onclick=function(){ 
+   optionOneScreen.style.display="block"
+
+    
+       
+                                    
+
+
+
+   
+};
+
+
